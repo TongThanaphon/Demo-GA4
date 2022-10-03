f9537f1342a043bc23e37fbc7cca5cb7eb1667bf
TongThanaphon <tongasura001@gmail.com>
2022-10-03 23:50:23 +0700

    update

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,9 +3,9 @@ import { MyText } from 'types/my-text'
 import React from 'react'
 
 const Text: React.FC<MyText> = (props) => {
-    const { text } = props
+  const { text } = props
 
-    return <p className='text-[#a1f542]'>Hello, {text}</p>
+  return <p className='text-[#a1f542]'>Hello, {text}</p>
 }
 
 export default Text
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 import Text from '@components/Text'
 
 const HomePage: NextPage = () => {
-    return (
-        <div>
-            <h1 className='text-3xl font-bold underline'>Hello World!!</h1>
-            <Text text='Tong' />
-        </div>
-    )
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello World!!</h1>
+      <Text text='Tong' />
+    </div>
+  )
 }
 
 export default HomePage
