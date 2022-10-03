3175baeb419a405a00ceda8a8e43bc9ceaa2d657
TongThanaphon <tongasura001@gmail.com>
2022-10-03 16:53:35 +0700

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+import React from 'react'
+
+import Text from '@components/Text'
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+      <Text />
+    </div>
+  )
+}
+
+export default HomePage
