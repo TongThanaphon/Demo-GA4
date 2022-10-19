ec0fe88ab2272119a85e6a6289d44e9f5ac07ced
TongThanaphon <tongasura001@gmail.com>
2022-10-19 15:54:50 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
-import Script from 'next/script'
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext,
@@ -20,32 +19,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-          {/* <!-- Google tag (gtag.js) --> */}
-          <Script
-            id='gtm'
-            async
-            src='https://www.googletagmanager.com/gtag/js?id=G-NN0TT0ZDRB'
-          ></Script>
-          <Script id='gtm-script'>
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-NN0TT0ZDRB');`}
-          </Script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
           {/* <!-- Google Tag Manager (noscript) --> */}
-          {/* <noscript>
+          <noscript>
             <iframe
               src='https://www.googletagmanager.com/ns.html?id=GTM-5F5GWC6'
               height='0'
               width='0'
               style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
-          </noscript> */}
+          </noscript>
           {/* <!-- End Google Tag Manager (noscript) --> */}
         </body>
       </Html>
